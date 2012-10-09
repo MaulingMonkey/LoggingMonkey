2535c026965308214bf8f374d990e5a3724e9996
Michael B. E. Rickert <nobody@nowhere.noplace>
2012-10-09 02:37:26 -0700

    Fix cookie expiration issue

--- HttpLogServer.cs
+++ HttpLogServer.cs
@@ -87,9 +87,10 @@ namespace LoggingMonkey {
 					var m = reAuthQuery.Match( context.Request.Url.Query ?? "" );
 					if( m.Success && m.Groups["token"].Success )
 					{
-						var cookie = new Cookie(Program.AuthCookieName,HttpUtility.UrlDecode(m.Groups["token"].Value));
-						context.Response.Cookies.Add(cookie);
-						acs = AccessControl.GetStatus( cookie.Value );
+						var expiration = DateTime.UtcNow.AddYears(10).ToString("ddd, dd-MMM-yyyy H:mm:ss"); // http://stackoverflow.com/questions/4811009/c-sharp-httplistener-cookies-expiring-after-session-even-though-expiration-time
+						var token = HttpUtility.UrlDecode(m.Groups["token"].Value);
+						context.Response.Headers.Add("Set-Cookie", string.Format("{0}={1};Path=/;Expires={2} GMT",Program.AuthCookieName,token,expiration));
+						acs = AccessControl.GetStatus(token);
 					}
 					break;
 				case "/robots.txt":
