b4ca26066e8c34d689407861928a926a46445e28
MaulingMonkey <nobody@nowhere.noplace>
2016-09-05 10:25:23 -0700

    s/MMTest/LoggingMonkey/

--- _Build/Build.ps1
+++ _Build/Build.ps1
@@ -6,8 +6,8 @@ $CONFIG	= $env:CONFIG
 $PLATFORM	= $env:PLATFORM
 
 status 'Building...'
-# TODO: Wrap in better MMTest build injestion tools
+# TODO: Wrap in better build injestion tools
 # Note that VS140 is VS 2015
-& "$env:VS140COMNTOOLS\..\IDE\devenv.com" MMTest.sln /build "$CONFIG|$PLATFORM"
+& "$env:VS140COMNTOOLS\..\IDE\devenv.com" LoggingMonkey.sln /build "$CONFIG|$PLATFORM"
 if (!$?) { error "Build FAILED" }
 else { status 'Built OK' }
