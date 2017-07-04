293d97017801670748f9928e5dea081a9f1c8135
MaulingMonkey <git@maulingmonkey.com>
2017-07-04 07:51:50 -0700

    Fix robots.txt not being served: resource was treated as a string instead of a byte[] because of the .txt extension apparently.

--- IO/Http/HttpLogServer/CreateHandleStaticFile.cs
+++ IO/Http/HttpLogServer/CreateHandleStaticFile.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Net;
+using System.Text;
 
 namespace LoggingMonkey {
 	partial class HttpLogServer {
 		private static Action<HttpRequest> CreateHandleStaticFile(string id)
 		{
-			var file = Assets.ResourceManager.GetObject(id) as byte[];
+			var obj = Assets.ResourceManager.GetObject(id);
+			var file = obj as byte[];
+			if (file == null) {
+				var txt = obj as string;
+				if (txt != null) file = Encoding.UTF8.GetBytes(txt);
+			}
+			if (file == null) throw new Exception("Missing static file: "+id);
 			return (request) => request.HttpListenerContext.Response.OutputStream.Write(file, 0, file.Length);
 		}
 	}
